e88a8c8009dd05af853b62cf7c5be3236942eed6
Beryl01 <berylnegesa@gmail.com>
2020-04-16 19:53:49 +0300

    Customized

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,16 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
   it(`should have as title 'IP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('IP');
   });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
